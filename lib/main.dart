beaa1aaf19697f18e786260ae5a2e1ba9c08ed99
rakesh <rakesh.das@idslogic.com>
2019-09-13 17:39:00 +0530

    column basics

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,50 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(
-    MaterialApp(
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
       home: Scaffold(
         backgroundColor: Colors.teal,
-        body: Container(),
+        body: SafeArea(
+            // By default column tries to take as much space as possible , vertically
+            child: Column(
+          // mainAxisSize: MainAxisSize.min,      Takes height of the children
+          // verticalDirection: VerticalDirection.up,        starts from bottom
+          // verticalDirection: VerticalDirection.down,        starts from top [default]
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          children: <Widget>[
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.white,
+              child: Center(
+                child: Text("Container 1"),
+              ),
+            ),
+            Container(
+              height: 100.0,
+              width: 100.0,
+              color: Colors.red,
+              child: Center(
+                child: Text("Container 2"),
+              ),
+            ),
+            Container(
+              height: 100.0,
+              width: double.infinity, // width 100%
+              color: Colors.yellow,
+              child: Center(
+                child: Text("Container 3"),
+              ),
+            ),
+          ],
+        )),
       ),
-    ),
-  );
+    );
+  }
 }
