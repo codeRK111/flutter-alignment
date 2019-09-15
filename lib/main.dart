b09d1ec7e45f09153f84ad634f588f6f7e3a9dff
Rakesh <prorakesh25956@gmail.com>
2019-09-15 16:30:25 -0700

    MI Card completed

--- lib/main.dart
+++ lib/main.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 import 'challange.dart';
 
 void main() {
-  runApp(Challange());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
@@ -17,30 +17,85 @@ class MyApp extends StatelessWidget {
           // mainAxisSize: MainAxisSize.min,      Takes height of the children
           // verticalDirection: VerticalDirection.up,        starts from bottom
           // verticalDirection: VerticalDirection.down,        starts from top [default]
-          crossAxisAlignment: CrossAxisAlignment.stretch,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          mainAxisAlignment: MainAxisAlignment.center,
+
           children: <Widget>[
-            Container(
-              height: 100.0,
-              width: 100.0,
+            CircleAvatar(
+              radius: 50.0,
+              backgroundImage: AssetImage("images/index.jpg"),
+            ),
+            Text(
+              "Rakesh Chandra Dash",
+              style: TextStyle(
+                  fontSize: 40.0,
+                  fontFamily: "Pacifico",
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "FLUTTER DEVELOPER",
+              style: TextStyle(
+                  fontSize: 20.0,
+                  letterSpacing: 5.0,
+                  fontFamily: "OpenSans",
+                  color: Colors.white,
+                  fontWeight: FontWeight.bold),
+            ),
+            SizedBox(
+              height: 20.0,
+              width: 150.0,
+              child: Divider(
+                color: Colors.teal.shade100,
+              ),
+            ),
+            Card(
               color: Colors.white,
-              child: Center(
-                child: Text("Container 1"),
+              margin: EdgeInsets.symmetric(horizontal: 25.0),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(Icons.phone, size: 20.0, color: Colors.teal),
+                    SizedBox(
+                      width: 30.0,
+                    ),
+                    Text(
+                      '9853325956',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
-            Container(
-              height: 100.0,
-              width: 100.0,
-              color: Colors.red,
-              child: Center(
-                child: Text("Container 2"),
-              ),
+            SizedBox(
+              height: 20.0,
             ),
-            Container(
-              height: 100.0,
-              width: double.infinity, // width 100%
-              color: Colors.yellow,
-              child: Center(
-                child: Text("Container 3"),
+            Card(
+              color: Colors.white,
+              margin: EdgeInsets.symmetric(horizontal: 25.0),
+              child: Padding(
+                padding: const EdgeInsets.all(10.0),
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.start,
+                  children: <Widget>[
+                    Icon(Icons.email, size: 20.0, color: Colors.teal),
+                    SizedBox(
+                      width: 30.0,
+                    ),
+                    Text(
+                      'rakeshchandrra@gmail.com',
+                      style: TextStyle(
+                        fontSize: 20.0,
+                        color: Colors.teal,
+                      ),
+                    )
+                  ],
+                ),
               ),
             ),
           ],
